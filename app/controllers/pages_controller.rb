5ae980ae7b33491bf132c4c5204ce8096f39d827
shoiraTa <shoira.shakirovna@gmail.com>
2022-07-18 15:34:11 +0400

    feat: home page form views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, :only=> [:index]
   def index
+    @popular_courses = Course.limit(3)
+    @top_rated_courses = Course.where('title ILIKE ?', '%design%')
+    @recent_courses = Course.all.limit(3).order(created_at: :desc)
   end
 end
