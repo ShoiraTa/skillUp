174d520f6b11c6162be6f41d74fc44d45dbf1122
shoiraTa <shoira.shakirovna@gmail.com>
2022-07-24 21:59:13 +0400

    refactor: activities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,7 +19,7 @@ class PagesController < ApplicationController
 
   def activity
     if current_user.has_role?(:admin)
-      @activities = PublicActivity::Activity.all
+      @pagy, @activities = pagy(PublicActivity::Activity.all.order(created_at: :desc))
     else
       redirect_to root_path, alert: "You are not authorised"
     end
