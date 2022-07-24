2ef5e76d5d3f33a28ae033b05da39fa6109bf9b9
shoiraTa <shoira.shakirovna@gmail.com>
2022-07-24 22:14:11 +0400

    feat: application helper for conditional styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 module ApplicationHelper
   include Pagy::Frontend
+def action_label(key)
+  case key
+    when 'create'
+      "<i class='fa fa-plus'></i>".html_safe
+    when 'update'
+      "<i class='fa fa-pen'></i>".html_safe
+    else 'delete'
+      "<i class='fa fa-trash'></i>".html_safe
+    end
+  end
+
+  def model_label(key)
+    case key
+      when 'Course'
+        "<i class='fa fa-graduation-cap'></i>".html_safe
+      when 'Lesson'
+        "<i class='fa fa-tasks'></i>".html_safe
+      end
+    end
 end
